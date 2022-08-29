64bff4b748af2108403582c8068f33e099224b60
kmc369 <keishacoleman710@gmail.com>
2022-08-29 14:25:43 -0400

    backend Done 90%

--- src/main/java/com/skillstorm/demo/controllers/goalController.java
+++ src/main/java/com/skillstorm/demo/controllers/goalController.java
@@ -2,6 +2,8 @@ package com.skillstorm.demo.controllers;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,8 +51,8 @@ public class goalController {
 	}
 	
 	@PostMapping
-	public goal save(@Valid @RequestBody goal goal) {
-		return service.save(goal);
+	public ResponseEntity<goal> save(@Valid @RequestBody goal goal) {
+		return new ResponseEntity<>(service.save(goal),HttpStatus.CREATED );
 	}
 	
 	@PutMapping("/{id}")
